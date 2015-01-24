4cb736f979e37d1affb3f1903933aba69a4535ae
IordacheClaudiu <klaudyu17@yahoo.com>
2015-01-24 14:17:03 +0200

    Add constants in EMUClient

--- EMUClient/src/ro/emu/client/utils/Constants.java
+++ EMUClient/src/ro/emu/client/utils/Constants.java
@@ -1,16 +1,6 @@
-package utils;
+package ro.emu.client.utils;
 
 public class Constants {
-	// Endpoints
-	public static final String dbpediaEndpoint = "http://dbpedia.org/sparql";
-	public static final String freebaseRDFResourceEndpoint = "http://rdf.freebase.com/ns/";
-	public static final String freebaseMQLEndpoint = "https://www.googleapis.com/freebase/v1/mqlread";
-
-	// Sparql prefixes
-	public static final String foafPrefix = "PREFIX foaf:<http://xmlns.com/foaf/0.1/>";
-	public static final String geoPrefix = "PREFIX geo:<http://www.w3.org/2003/01/geo/wgs84_pos#>";
-	public static final String dbppropPrefix = "PREFIX dbpprop:<http://dbpedia.org/property/>";
-	public static final String dbpediaOwlPrefix = "PREFIX dbpedia-owl:<http://dbpedia.org/ontology/>";
 
 	// RDF Namespace
 	public static final String rdf = "http://www.w3.org/1999/02/22-rdf-syntax-ns#";
